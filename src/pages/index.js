c1411617ee12264c5739d1d8b96927077cdad992
ORiveraJr84 <oriverajr84@gmail.com>
2021-09-04 21:38:14 -0400

    added a pic of clifford

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { Link } from "gatsby";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const App = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this page following the Gatsby tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   );
 };
